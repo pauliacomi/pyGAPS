379b13d1b9532f095829f6cf6319b1050f57f8ec
Paul Iacomi <iacomi.paul@gmail.com>
2017-07-30 21:31:45 +0200

    added bokeh to dependency list

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'scipy >= 0.18.0',
         'pandas >= 0.18.1',
         'matplotlib >= 2.0',
+        'bokeh'
         'pyiast',
         'xlwings;platform_system=="Windows"',
         'coolprop >= 6.0',
