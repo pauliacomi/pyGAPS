037571a2d45679296a712e79f6888eb86862fc38
Paul Iacomi <iacomi.paul@gmail.com>
2017-07-30 21:03:28 +0200

    commented out unused code

--- src/adsutils/graphing/iastgraphs.py
+++ src/adsutils/graphing/iastgraphs.py
@@ -1,67 +1,60 @@
-# %%
-import os
-import numpy
-import matplotlib.pyplot as plt
-# chose an implementation, depending on os
-if os.name == 'nt':  # sys.platform == 'win32':
-    import xlwings as xw
-else:
-    xw = None
-    raise Warning(
-        "xlwings functionality disabled on this platform ( {0} )".format(os.name))
+# # %%
+# import os
+# import numpy
+# import matplotlib.pyplot as plt
+
+# # chose an implementation, depending on os
+# import xlwings as xw
 
 
-def plot_iast_vle():
-    "Function to generate the selectivity graph"
+# def plot_iast_vle():
+#     "Function to generate the selectivity graph"
 
-    # get excel workbook, sheet and range
-    wb = xw.Book
-    sht = xw.sheets[3]
-    data_1 = sht.range('A1').options(numpy.array, expand='table').value
-    data_2 = sht.range('D1').options(numpy.array, expand='table').value
-    data_3 = sht.range('G1').options(numpy.array, expand='table').value
+#     # get excel workbook, sheet and range
+#     wb = xw.Book
+#     sht = xw.sheets[3]
+#     data_1 = sht.range('A1').options(numpy.array, expand='table').value
+#     data_2 = sht.range('D1').options(numpy.array, expand='table').value
+#     data_3 = sht.range('G1').options(numpy.array, expand='table').value
 
-    # split into multiple
-    data_1_x, data_1_y = numpy.hsplit(data_1, 2)
-    data_2_x, data_2_y = numpy.hsplit(data_2, 2)
-    data_3_x, data_3_y = numpy.hsplit(data_3, 2)
+#     # split into multiple
+#     data_1_x, data_1_y = numpy.hsplit(data_1, 2)
+#     data_2_x, data_2_y = numpy.hsplit(data_2, 2)
+#     data_3_x, data_3_y = numpy.hsplit(data_3, 2)
 
-    text_x = r'y $CO_2$'
-    text_y = r'x $CO_2$'
-    # title_graph = r'$CO_2$ in $N_2$, 303K'
-    # title_graph = r'$CO_2$ in $CH_4$, 303K'
-    title_graph = r'$C_3H_6$ in $C_3H_8$, 303K'
+#     text_x = r'y $CO_2$'
+#     text_y = r'x $CO_2$'
+#     # title_graph = r'$CO_2$ in $N_2$, 303K'
+#     # title_graph = r'$CO_2$ in $CH_4$, 303K'
+#     title_graph = r'$C_3H_6$ in $C_3H_8$, 303K'
 
-    fig, ax = plt.subplots(figsize=(8, 8))
+#     fig, ax = plt.subplots(figsize=(8, 8))
 
-    # graph title
-    plt.title(title_graph, fontsize=22, ha='center', va='bottom')
+#     # graph title
+#     plt.title(title_graph, fontsize=22, ha='center', va='bottom')
 
-    # labels for the axes
-    plt.xlabel(text_x, fontsize=15)
-    plt.ylabel(text_y, fontsize=15)
+#     # labels for the axes
+#     plt.xlabel(text_x, fontsize=15)
+#     plt.ylabel(text_y, fontsize=15)
 
-    plt.plot(data_1_x, data_1_y, label='1 bar')
-    plt.plot(data_2_x, data_2_y, label='5 bar')
-    plt.plot(data_3_x, data_3_y, label='10 bar')
+#     plt.plot(data_1_x, data_1_y, label='1 bar')
+#     plt.plot(data_2_x, data_2_y, label='5 bar')
+#     plt.plot(data_3_x, data_3_y, label='10 bar')
 
-    # Straight line
-    line = [0, 1]
-    plt.plot(line, line, color='black')
+#     # Straight line
+#     line = [0, 1]
+#     plt.plot(line, line, color='black')
 
-    plt.legend(fontsize=15, loc='lower right')
+#     plt.legend(fontsize=15, loc='lower right')
 
-    ax.grid(True, zorder=5)
-    ax.set_xlim(xmin=0, xmax=1)
-    ax.set_ylim(ymin=0, ymax=1)
-    ax.set_xscale('linear')
+#     ax.grid(True, zorder=5)
+#     ax.set_xlim(xmin=0, xmax=1)
+#     ax.set_ylim(ymin=0, ymax=1)
+#     ax.set_xscale('linear')
 
-    title = title_graph + '.png'
-    plt.savefig(title, transparent=False)
+#     title = title_graph + '.png'
+#     plt.savefig(title, transparent=False)
 
-    plt.show()
+#     plt.show()
 
-    return
-
-
-plot_iast_vle()
+#     return
---
+++ tests/test_imports.py
@@ -0,0 +1,12 @@
+import pytest
+import sys
+
+
+windows = pytest.mark.skipif(
+    sys.platform != 'win32', reason="requires windows")
+
+
+@windows
+def test_func_skipped():
+    """Test the function"""
+    assert 0
