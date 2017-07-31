b30921c1c8ad860c9cc2fd79698c3dfc05d5f508
Paul Iacomi <iacomi.paul@gmail.com>
2017-07-31 17:39:12 +0200

    updated tox.ini with python 3.5/3.6, removed pypy

---
+++ src/adsutils/dataimport/jsoninterface.py
@@ -0,0 +1,42 @@
+# # %%
+
+# import json
+# import urllib
+
+# material = 'CuBTC'
+
+# with urllib.request.urlopen("http://adsorbents.nist.gov/api/material-biblio/" + material + ".json") as url:
+#     data = json.loads(url.read().decode())
+
+# # %%
+# dois = [x['DOI'] for x in data]
+# dois = [x[:7] + x[8:] for x in dois]
+# print(dois[0])
+
+# # %%
+# exps = []
+
+# total_dois = len(dois)
+# for index, doi in enumerate(dois):
+#     print("Starting doi number ", index, "out of ", total_dois)
+
+#     iso_number = 1
+#     while True:
+#         print("    Starting isotherm number ", iso_number)
+
+#         title = dois[index] + ".isotherm" + str(iso_number)
+#         iso_number += 1
+#         response = urllib.request.urlopen(
+#             "http://adsorbents.nist.gov/api/isotherm/" + title + ".json")
+
+#         iso_data = json.loads(response.read().decode())
+#         if len(iso_data) > 0:
+#             exps.append(iso_data)
+#         else:
+#             break
+
+# # %%
+# print(len(exps))
+
+# with open('result.json', 'w') as fp:
+#     json.dump(exps, fp, indent=4)
