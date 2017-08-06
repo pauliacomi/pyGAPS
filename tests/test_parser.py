68cacbad71d6ec54fd6e3c2cb6830f01c96feb16
Paul Iacomi <iacomi.paul@gmail.com>
2017-08-07 01:50:50 +0200

    updated parser test

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -38,13 +38,6 @@ def test_isotherm_to_json(basic_isotherm, basic_isotherm_json):
 def test_isotherm_from_json(basic_isotherm, basic_isotherm_json):
 
     test_isotherm = adsutils.isotherm_from_json(basic_isotherm_json)
-
-    assert basic_isotherm.id == test_isotherm.id
-    assert basic_isotherm.sample_name == test_isotherm.sample_name
-    assert basic_isotherm.sample_batch == test_isotherm.sample_batch
-    assert basic_isotherm.t_exp == test_isotherm.t_exp
-    assert basic_isotherm.gas == test_isotherm.gas
-
-    assert basic_isotherm.data().equals(test_isotherm.data())
+    assert basic_isotherm == test_isotherm
 
     return
