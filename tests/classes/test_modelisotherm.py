46fa3ea3de2d4b057758c297b2ca41305d0b0cb4
Paul Iacomi <iacomi.paul@gmail.com>
2017-10-04 17:17:05 +0200

    added test for modelisotherm

--- tests/classes/test_modelisotherm.py
+++ tests/classes/test_modelisotherm.py
@@ -4,6 +4,7 @@ This test module has tests relating to the adsorbate class
 
 import os
 
+import pandas
 import pytest
 from matplotlib.testing.decorators import cleanup
 
@@ -18,10 +19,36 @@ class TestModelIsotherm(object):
     Tests the pointisotherm class
     """
 
+    def test_isotherm_create(self):
+        "Checks isotherm can be created from basic data"
+        isotherm_param = {
+            'sample_name': 'carbon',
+            'sample_batch': 'X1',
+            'adsorbate': 'nitrogen',
+            't_exp': 77,
+        }
+
+        isotherm_data = pandas.DataFrame({
+            'pressure': [1, 2, 3, 4, 5, 3, 2],
+            'loading': [1, 2, 3, 4, 5, 3, 2]
+        })
+
+        isotherm = pygaps.ModelIsotherm(
+            isotherm_data,
+            loading_key='loading',
+            pressure_key='pressure',
+            model='Henry',
+            param_guess=None,
+            optimization_method="Nelder-Mead",
+            **isotherm_param
+        )
+
+        return isotherm
+
     @pytest.mark.parametrize('file, ',
                              [(data['file']) for data in list(DATA.values())])
-    def test_isotherm_create(self, file):
-        "Checks isotherm can be created from test data"
+    def test_isotherm_create_pointiso(self, file):
+        "Checks isotherm can be created from a pointisotherm"
 
         filepath = os.path.join(DATA_PATH, file)
 
