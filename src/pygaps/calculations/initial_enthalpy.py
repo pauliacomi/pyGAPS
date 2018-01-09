3fd60fdc699ff88c3e96cd69958b8f20657aa619
Paul Iacomi <iacomi.paul@gmail.com>
2018-01-09 13:59:44 +0100

    reasonable results (exp + const + attr + rep)

--- src/pygaps/calculations/initial_enthalpy.py
+++ src/pygaps/calculations/initial_enthalpy.py
@@ -35,8 +35,8 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
 
     Returns
     -------
-    float
-        Initial enthalpy.
+    dict
+        Dict containing initial enthalpy and fitting parameters.
     """
 
     # Read data in
@@ -60,20 +60,27 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
 
     ##################################
     ##################################
-    # First define the parameters and functions
+    # First define the parameters
 
-    param_names = ['const', 'preexp', 'exp', 'prepow', 'pow']
+    param_names = ['const', 'preexp', 'exp',
+                   'prepowa', 'powa', 'prepowr', 'powr']
     params = {name: numpy.nan for name in param_names}
 
     # Then the functions
-    def enthalpy_approx(loading_):
+    def constant_term(l):
+        return params['const']
 
-        term_constant = params['const']
-        term_exponential = params['preexp'] * \
-            numpy.exp(params['exp'] * loading_)
-        term_power = params['prepow'] * loading_ ** params['pow']
+    def exponential_term(l):
+        return params['preexp'] * numpy.exp(params['exp'] * l)
 
-        return term_constant + term_exponential + term_power
+    def power_term_repulsive(l):
+        return params['prepowr'] * l ** params['powr']
+
+    def power_term_attractive(l):
+        return params['prepowa'] * l ** params['powa']
+
+    def enthalpy_approx(l):
+        return constant_term(l) + exponential_term(l) + power_term_repulsive(l) + power_term_attractive(l)
 
     def residual_sum_of_squares(params_):
         for i, _ in enumerate(param_names):
@@ -90,24 +97,27 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
     ##################################
     # The constant term
 
+    # We calculate the standard deviation
+    enth_avg = numpy.average(enthalpy)
+    enth_stdev = numpy.std(enthalpy)
+
     # The minimum should be at least the enthalpy of liquefaction
     # although it depends on the strength of the interaction with the
     # active site.
-    # We assume enthalpy of liquefaction
+
+    bounds['const_min'] = enth_avg - 2 * enth_stdev
+    # We check enthalpy of liquefaction
     adsorbate = Adsorbate.from_list(isotherm.adsorbate)
     try:
-        bounds['const_min'] = adsorbate.enthalpy_liquefaction(isotherm.t_exp)
+        bounds['const_min'] = max(adsorbate.enthalpy_liquefaction(
+            isotherm.t_exp), bounds['const_min'])
     except CalculationError as e_info:
         warnings.warn(
             "Could not calculate liquid enthalpy, perhaps in supercritical regime")
-        bounds['const_min'] = 0
 
-    # The maximum constant contribution should be in the average
-    # of the enthalpies. However, because the contribution from
-    # guest-guest interactions could impact it highly,
-    # we instead consider a multiple of the average
-    const_avg = max(numpy.average(enthalpy), bounds['const_min'])
-    bounds['const_max'] = const_avg * 1.2
+    # The maximum constant contribution is taken similar to the minimum
+    const_avg = max(enth_avg, bounds['const_min'])
+    bounds['const_max'] = const_avg
 
     ##################################
     # The exponential term
@@ -128,19 +138,24 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
     # be the preexponential factor + the constant factor.
     # Physically, there must be a limit for this interaction
     # even for chemisorption. We set a conservative limit.
-    bounds['preexp_max'] = 300
+    bounds['preexp_max'] = 150
 
     ##################################
     # The power term
 
     # The power term is supposed to model the guest-guest interactions
     # with two components: the power and the coefficient
-    # The power cannot be negative
-    bounds['pow_min'] = 0
-    bounds['pow_max'] = numpy.inf
+    # The power should be at least 1 (1-1 interactions)
+    bounds['powa_min'] = 1
+    bounds['powr_min'] = 1
+    # We set a realistic upper limit on the number of interactions
+    bounds['powa_max'] = 50
+    bounds['powr_max'] = 50
 
-    bounds['prepow_min'] = -numpy.inf
-    bounds['prepow_max'] = numpy.inf
+    bounds['prepowa_min'] = 0
+    bounds['prepowa_max'] = numpy.inf
+    bounds['prepowr_min'] = -numpy.inf
+    bounds['prepowr_max'] = 0
     # ###############
     # Update with user values
     bounds.update(param_guess)
@@ -150,13 +165,23 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
         (bounds.get('const_min'), bounds.get('const_max')),
         (bounds.get('preexp_min'), bounds.get('preexp_max')),
         (bounds.get('exp_min'), bounds.get('exp_max')),
-        (bounds.get('prepow_min'), bounds.get('prepow_max')),
-        (bounds.get('pow_min'), bounds.get('pow_max')),
+        (bounds.get('prepowa_min'), bounds.get('prepowa_max')),
+        (bounds.get('powa_min'), bounds.get('powa_max')),
+        (bounds.get('prepowr_min'), bounds.get('prepowr_max')),
+        (bounds.get('powr_min'), bounds.get('powr_max')),
     )
 
     if verbose:
         print('Bounds: \n\tconst =', (bounds_arr[0]), ', preexp =', bounds_arr[1],
-              ', exp =', bounds_arr[2], ', prepow =', bounds_arr[3], ', pow =', bounds_arr[4])
+              ', exp =', bounds_arr[2], ', prepowa =', bounds_arr[3], ', powa =', bounds_arr[4],
+              ', prepowr =', bounds_arr[5], ', powr =', bounds_arr[6])
+
+    ##################################
+    ##################################
+    # Constraints on the parameters
+    def maximize_constant(params_):
+        return params_[0] - params_[1] * numpy.exp(params_[2] * loading) - params_[3] * loading ** params_[4] - params_[5] * loading ** params_[6]
+    constr = ({'type': 'ineq', 'fun': maximize_constant})
 
     ##################################
     ##################################
@@ -168,18 +193,21 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
     dep_last = min(max(enthalpy[-1], 0), 150) - const_avg
     guesses = (
         # Starting from a constant value
-        numpy.array([const_avg, 0, 0, 0, 1]),
+        numpy.array([const_avg, 0, 0, 0, 1, 0, 1]),
         # Starting from an adjusted start and end
         numpy.array([const_avg,
                      dep_first, 0,
+                     dep_last, 1,
                      dep_last, 1]),
         # Starting from a large exponent and gentle power increase
         numpy.array([const_avg,
                      1.5 * dep_first, -10,
-                     0.01, 3]),
+                     0.01, 3,
+                     0, 1]),
         # Starting from no exponent and gentle power decrease
         numpy.array([const_avg,
                      0, 0,
+                     0, 3,
                      -0.01, 3]),
     )
 
@@ -196,9 +224,10 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
     for guess in guesses:
         if verbose:
             print('Initial guesses: \n\tconst =', guess[0], ', preexp =', guess[1],
-                  ', exp =', guess[2], ', prepow =', guess[3], ', pow =', guess[4])
+                  ', exp =', guess[2], ', prepowa =', guess[3], ', powa =', guess[4],
+                  ', prepowr =', guess[5], ', powr =', guess[6])
         opt_res = scipy.optimize.minimize(residual_sum_of_squares, guess,
-                                          bounds=bounds_arr,
+                                          bounds=bounds_arr, constraints=constr,
                                           method='SLSQP', options=options)
 
         if opt_res.fun < min_fun:
@@ -219,7 +248,7 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
         warnings.warn(
             "Probable offshoot for exponent, reverting to point method")
         initial_enthalpy = initial_enthalpy_point(
-            isotherm, enthalpy_key, branch=branch, verbose=verbose)
+            isotherm, enthalpy_key, branch=branch, verbose=verbose).get('initial_enthalpy')
 
     if verbose:
         print("The initial enthalpy of adsorption is: \n\tE =",
@@ -228,15 +257,28 @@ def initial_enthalpy_comp(isotherm, enthalpy_key, branch='ads', verbose=False, *
             params['const']))
         print("The exponential contribution is \n\t{:.2f} * exp({:.2E} * n)".format(
             params['preexp'], params['exp']))
-        print("The power contribution is \n\t{:.2g} * n^{:.2}".format(
-            params['prepow'], params['pow']))
+        print("The guest-guest attractive contribution is \n\t{:.2g} * n^{:.2}".format(
+            params['prepowa'], params['powa']))
+        print("The guest-guest repulsive contribution is \n\t{:.2g} * n^{:.2}".format(
+            params['prepowr'], params['powr']))
 
+        x_axis = numpy.linspace(0, 1)
+        baseline = constant_term(x_axis)
+        extras = (
+            (x_axis, [baseline for x in x_axis], 'constant'),
+            (x_axis, baseline + exponential_term(x_axis), 'exponential'),
+            (x_axis, baseline + power_term_attractive(x_axis), 'power attr'),
+            (x_axis, baseline + power_term_repulsive(x_axis), 'power rep'),
+        )
+
+        title = ' '.join(
+            [isotherm.sample_name, isotherm.sample_batch, isotherm.adsorbate])
         initial_enthalpy_plot(
-            loading, enthalpy, enthalpy_approx(loading))
+            loading, enthalpy, enthalpy_approx(loading), title=title, extras=extras)
 
-        return initial_enthalpy, params
+    params.update({'initial_enthalpy': initial_enthalpy})
 
-    return initial_enthalpy
+    return params
 
 
 def initial_enthalpy_point(isotherm, enthalpy_key, branch='ads', verbose=False):
@@ -257,8 +299,8 @@ def initial_enthalpy_point(isotherm, enthalpy_key, branch='ads', verbose=False):
 
     Returns
     -------
-    float
-        Initial enthalpy.
+    dict
+        Dict containing initial enthalpy and other parameters.
     """
 
     # Read data in
@@ -273,4 +315,12 @@ def initial_enthalpy_point(isotherm, enthalpy_key, branch='ads', verbose=False):
         print("The initial enthalpy of adsorption is: \n\tE =",
               round(initial_enthalpy, 2))
 
-    return initial_enthalpy
+        loading = isotherm.loading(branch=branch,
+                                   loading_unit='mmol',
+                                   loading_basis='molar')
+        title = ' '.join(
+            [isotherm.sample_name, isotherm.sample_batch, isotherm.adsorbate])
+        initial_enthalpy_plot(
+            loading, enthalpy, [initial_enthalpy for i in loading], title=title)
+
+    return {'initial_enthalpy': initial_enthalpy}
