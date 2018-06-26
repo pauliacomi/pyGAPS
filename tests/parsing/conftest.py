901d962091e11c5595bdf3e1387b215d22b629ad
Paul Iacomi <iacomi.paul@gmail.com>
2018-06-26 19:51:41 +0200

    testing for xlrd/xlwt

--- tests/parsing/conftest.py
+++ tests/parsing/conftest.py
@@ -5,6 +5,10 @@ DATA_PATH = os.path.join(
     'docs', 'examples', 'data', 'parsing')
 
 DATA_EXCEL_PATH = os.path.join(DATA_PATH, 'excel')
+
+DATA_EXCEL_STD = [os.path.join(DATA_EXCEL_PATH, 'HKUST-1(Cu) CO2 303.0.xls'),
+                  os.path.join(DATA_EXCEL_PATH, 'MCM-41 N2 77.0.xls')]
+
 DATA_EXCEL_MIC = [os.path.join(DATA_EXCEL_PATH, 'mic', 'Sample_A.xls'),
                   os.path.join(DATA_EXCEL_PATH, 'mic', 'Sample_B.xls')]
 
--- tests/parsing/test_excel.py
+++ tests/parsing/test_excel.py
@@ -7,45 +7,25 @@ import pytest
 import pygaps
 
 from ..conftest import parsing
-from ..conftest import windows
 from .conftest import DATA_EXCEL_BEL
 from .conftest import DATA_EXCEL_MIC
+from .conftest import DATA_EXCEL_STD
 
 
-@windows
 @parsing
 @pytest.mark.slowtest
 class TestExcel(object):
 
-    def test_create_excel(self, basic_pointisotherm, tmpdir_factory):
-        """Tests creation of the regular excel file"""
+    def test_read_excel(self):
+        """Tests reading of bel report files"""
 
-        path = tmpdir_factory.mktemp('excel').join('regular.xlsx').strpath
-        try:
-            pygaps.isotherm_to_xl(basic_pointisotherm,
-                                  path=path)
-        except SystemError:
-            # Excel is not installed
-            return
+        for path in DATA_EXCEL_STD:
+            isotherm = pygaps.isotherm_from_xl(path=path)
 
-        isotherm = pygaps.isotherm_from_xl(path)
+            json_path = path.replace('.xlsx', '.json')
 
-        assert isotherm == basic_pointisotherm
-
-    def test_create_excel_madirel(self, basic_pointisotherm, tmpdir_factory):
-        """Tests creation of the MADIREL file"""
-
-        path = tmpdir_factory.mktemp('excel').join('MADIREL.xlsx').strpath
-        try:
-            pygaps.isotherm_to_xl(basic_pointisotherm,
-                                  path=path, fmt='MADIREL')
-        except SystemError as e_info:
-            # Excel is not installed
-            return
-
-        isotherm = pygaps.isotherm_from_xl(path, fmt='MADIREL')
-
-        assert isotherm == basic_pointisotherm
+            with open(json_path, 'r') as file:
+                assert pygaps.isotherm_to_json(isotherm) == file.read()
 
     def test_read_excel_mic(self):
         """Tests reading of micromeritics report files"""
@@ -70,3 +50,25 @@ class TestExcel(object):
 
             with open(json_path, 'r') as file:
                 assert pygaps.isotherm_to_json(isotherm) == file.read()
+
+    def test_create_excel(self, basic_pointisotherm, tmpdir_factory):
+        """Tests creation of the regular excel file"""
+
+        path = DATA_EXCEL_STD[0]
+        # tmpdir_factory.mktemp('excel').join('regular.xls').strpath
+        pygaps.isotherm_to_xl(basic_pointisotherm, path=path)
+
+        isotherm = pygaps.isotherm_from_xl(path)
+
+        assert isotherm == basic_pointisotherm
+
+    def test_create_excel_madirel(self, basic_pointisotherm, tmpdir_factory):
+        """Tests creation of the MADIREL file"""
+
+        path = tmpdir_factory.mktemp('excel').join('MADIREL.xlsx').strpath
+        pygaps.isotherm_to_xl(basic_pointisotherm,
+                              path=path, fmt='MADIREL')
+
+        isotherm = pygaps.isotherm_from_xl(path, fmt='MADIREL')
+
+        assert isotherm == basic_pointisotherm
