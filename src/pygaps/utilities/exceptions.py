eb77da9dcebd7774e0377c16c74c6a1c2400969d
Paul Iacomi <pauliacomi@users.noreply.github.com>
2018-09-24 17:04:00 +0200

    renamed error

--- src/pygaps/utilities/exceptions.py
+++ src/pygaps/utilities/exceptions.py
@@ -3,21 +3,21 @@ Custom errors thrown by the program.
 """
 
 
-class Error(Exception):
+class pgError(Exception):
     """Error raised by this program."""
 
 
-class ParameterError(Error):
+class ParameterError(pgError):
     """Raised when one of the parameters is unsuitable."""
 
 
-class CalculationError(Error):
+class CalculationError(pgError):
     """Raised when a calculation fails."""
 
 
-class ParsingError(Error):
+class ParsingError(pgError):
     """Raised when parsing fails."""
 
 
-class GraphingError(Error):
+class GraphingError(pgError):
     """Raised when graphing fails."""
