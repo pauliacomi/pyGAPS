3e1d4bd79c8bc16440b0103a5378dd0b91597fb8
Paul Iacomi <pauliacomi@users.noreply.github.com>
2018-12-12 23:00:13 +0100

    simplified excel madirel testing

--- tests/parsing/test_excel.py
+++ tests/parsing/test_excel.py
@@ -26,30 +26,13 @@ class TestExcel(object):
     def test_read_create_excel_madirel(self, basic_pointisotherm, tmpdir_factory):
         """Tests creation of the MADIREL file"""
 
-        iso_dict = basic_pointisotherm.to_dict()
-        for key in pygaps.parsing.excelinterface._FIELDS_MADIREL:
-            if not iso_dict.get(key):
-                iso_dict[key] = ''
-
-        for key in pygaps.parsing.excelinterface._FIELDS_MADIREL_ENTH:
-            if not iso_dict.get(key):
-                iso_dict[key] = ''
-
-        iso_data = basic_pointisotherm.raw_data
-
-        isotherm = pygaps.PointIsotherm(iso_data,
-                                        loading_key=basic_pointisotherm.loading_key,
-                                        pressure_key=basic_pointisotherm.pressure_key,
-                                        other_keys=basic_pointisotherm.other_keys,
-                                        **iso_dict)
-
         path = DATA_EXCEL_STD[0]
         path = tmpdir_factory.mktemp('excel').join('MADIREL.xlx').strpath
-        pygaps.isotherm_to_xl(isotherm,
+        pygaps.isotherm_to_xl(basic_pointisotherm,
                               path=path, fmt='MADIREL')
 
         from_iso = pygaps.isotherm_from_xl(path, fmt='MADIREL')
-        assert from_iso == isotherm
+        assert from_iso == basic_pointisotherm
 
     def test_read_excel_mic(self):
         """Tests reading of micromeritics report files"""
