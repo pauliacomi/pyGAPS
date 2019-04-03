2c91e5c4986b9bed29d37b63b7e7306100ecc8fb
Paul Iacomi <pauliacomi@users.noreply.github.com>
2019-04-03 18:16:52 +0200

    testing coolprop interactions and removing unnecessary material backends

--- tests/utilities/test_coolprop_interaction.py
+++ tests/utilities/test_coolprop_interaction.py
@@ -7,3 +7,17 @@ def test_backend_change():
     previous_backend = pygaps.COOLPROP_BACKEND
     pygaps.backend_use_refprop()
     assert previous_backend != pygaps.COOLPROP_BACKEND
+
+
+def test_backend_names():
+    for adsorbate in pygaps.ADSORBATE_LIST:
+        try:
+            adsorbate.backend.molar_mass()
+        except pygaps.utilities.exceptions.ParameterError:
+            pass
+        pygaps.backend_use_refprop()
+        try:
+            adsorbate.backend.molar_mass()
+        except pygaps.utilities.exceptions.ParameterError:
+            pass
+        pygaps.backend_use_coolprop()
