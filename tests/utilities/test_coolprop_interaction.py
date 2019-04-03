830acf9170dc481a9f3c385a230948aa7144005a
Paul Iacomi <pauliacomi@users.noreply.github.com>
2019-04-03 20:37:32 +0200

    test run only if refprop

--- tests/utilities/test_coolprop_interaction.py
+++ tests/utilities/test_coolprop_interaction.py
@@ -2,6 +2,8 @@
 
 import pygaps
 
+import CoolProp
+
 
 def test_backend_change():
     previous_backend = pygaps.COOLPROP_BACKEND
@@ -9,15 +11,21 @@ def test_backend_change():
     assert previous_backend != pygaps.COOLPROP_BACKEND
 
 
-def test_backend_names():
+def test_backend_names_coolprop():
     for adsorbate in pygaps.ADSORBATE_LIST:
         try:
             adsorbate.backend.molar_mass()
         except pygaps.utilities.exceptions.ParameterError:
             pass
-        pygaps.backend_use_refprop()
-        try:
-            adsorbate.backend.molar_mass()
-        except pygaps.utilities.exceptions.ParameterError:
-            pass
-        pygaps.backend_use_coolprop()
+
+
+def test_backend_names_refprop():
+    version = CoolProp.CoolProp.get_global_param_string("REFPROP_version")
+    if version:
+        for adsorbate in pygaps.ADSORBATE_LIST:
+            pygaps.backend_use_refprop()
+            try:
+                adsorbate.backend.molar_mass()
+            except pygaps.utilities.exceptions.ParameterError:
+                pass
+            pygaps.backend_use_coolprop()
