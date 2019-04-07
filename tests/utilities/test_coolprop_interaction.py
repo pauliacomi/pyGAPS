3a9af97ffa3aef26570f0f9e50f204430ad491f8
Paul Iacomi <pauliacomi@users.noreply.github.com>
2019-04-07 02:44:38 +0200

    fixed error in backend test

--- tests/utilities/test_coolprop_interaction.py
+++ tests/utilities/test_coolprop_interaction.py
@@ -5,28 +5,35 @@ import CoolProp
 import pygaps
 
 
-def test_backend_change():
-    previous_backend = pygaps.COOLPROP_BACKEND
-    pygaps.backend_use_refprop()
-    assert previous_backend != pygaps.COOLPROP_BACKEND
+class TestCoolProp():
+    """Test CoolProp interaction."""
 
+    def test_backend_change(self):
+        """Test if backend can change."""
+        previous_backend = pygaps.COOLPROP_BACKEND
+        pygaps.backend_use_refprop()
+        assert previous_backend != pygaps.COOLPROP_BACKEND
+        pygaps.backend_use_coolprop()
+        assert previous_backend == pygaps.COOLPROP_BACKEND
 
-def test_backend_names_coolprop():
-    for adsorbate in pygaps.ADSORBATE_LIST:
-        try:
-            adsorbate.backend.molar_mass()
-        except pygaps.utilities.exceptions.ParameterError:
-            pass
-
-
-def test_backend_names_refprop():
-    version = CoolProp.CoolProp.get_global_param_string("REFPROP_version")
-    if version:
-        print(version)
+    def test_backend_names_coolprop(self):
+        """Test if CoolProp can be called for database adsorbents."""
         for adsorbate in pygaps.ADSORBATE_LIST:
-            pygaps.backend_use_refprop()
             try:
                 adsorbate.backend.molar_mass()
             except pygaps.utilities.exceptions.ParameterError:
                 pass
+
+    def test_backend_names_refprop(self):
+        """Test if RERFPROP can be called for database adsorbents."""
+        version = CoolProp.CoolProp.get_global_param_string("REFPROP_version")
+        if version:
+            print(version)
+            pygaps.backend_use_refprop()
+            for adsorbate in pygaps.ADSORBATE_LIST:
+                print(adsorbate)
+                try:
+                    adsorbate.backend.molar_mass()
+                except pygaps.utilities.exceptions.ParameterError:
+                    pass
             pygaps.backend_use_coolprop()
