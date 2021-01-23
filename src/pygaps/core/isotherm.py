ab7608f83f274d0b5356632065d149294c8c4f62
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-01-23 17:01:04 +0100

    limited hashing precision to account for float limit in pandas<1.2

--- src/pygaps/core/isotherm.py
+++ src/pygaps/core/isotherm.py
@@ -186,7 +186,7 @@ class Isotherm():
             setattr(self, attr, properties[attr])
 
     ##########################################################
-    #   Overloaded and private functions
+    #   Overloaded and own functions
 
     @property
     def iso_id(self):
