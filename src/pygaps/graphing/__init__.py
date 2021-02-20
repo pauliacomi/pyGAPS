1d123ad6232e0da4c8e9f8d92bc4081f3293efdd
Paul Iacomi <pauliacomi@users.noreply.github.com>
2021-02-20 19:16:31 +0100

    fix lazy loading for 3.6

--- src/pygaps/graphing/__init__.py
+++ src/pygaps/graphing/__init__.py
@@ -3,13 +3,27 @@
 
 from .. import _load_lazy
 
-
 class mpl_backend():
     """A backend for matplotlib, which will be lazy loaded eventually."""
     def __init__(self):
-        self.pyplot = _load_lazy('matplotlib.pyplot')
-        self.ticker = _load_lazy('matplotlib.ticker')
-        self.cm = _load_lazy('matplotlib.cm')
+        pass
+        # self.pyplot = _load_lazy('matplotlib.pyplot')
+        # self.ticker = _load_lazy('matplotlib.ticker')
+        # self.cm = _load_lazy('matplotlib.cm')
 
+    @property
+    def pyplot(self):
+        import matplotlib.pyplot as plt
+        return plt
+
+    @property
+    def ticker(self):
+        import matplotlib.ticker as tick
+        return tick
+
+    @property
+    def cm(self):
+        import matplotlib.cm as cm
+        return cm
 
 plt = mpl_backend()
