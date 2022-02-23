63de971ecd6b14a03dba7c8a7e139def92895d73
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-02-23 23:44:20 +0100

    '3.1.0 → 4.0.0'

--- src/pygaps/__init__.py
+++ src/pygaps/__init__.py
@@ -9,7 +9,7 @@ try:
     from ._version import version
     __version__ = version
 except ImportError:
-    __version__ = '3.1.0'
+    __version__ = '4.0.0'
 
 import sys
 from .logging import logger
