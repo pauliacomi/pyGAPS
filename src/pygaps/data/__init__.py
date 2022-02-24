5aa4d4781f7dc3e098eb448948a817ebfb3c8adf
Paul Iacomi <pauliacomi@users.noreply.github.com>
2022-02-24 14:09:13 +0100

    fix: importlib_resources.files for py<3.9

--- src/pygaps/data/__init__.py
+++ src/pygaps/data/__init__.py
@@ -8,14 +8,22 @@ internal database location.
 # flake8: noqa
 # isort:skip_file
 
-import importlib.resources as importlib_resources
+try:
+    import importlib.resources as importlib_resources
+    from importlib.resources import files as importlib_resources_files
+except ImportError:
+    # TODO Deprecation after PY<39
+    # Use backported `importlib_resources`.
+    import importlib_resources as importlib_resources
+    from importlib_resources import files as importlib_resources_files
+
 from contextlib import ExitStack
 import atexit
 
 # We use an exit stack and register it at interpreter exit to cleanup anything needed
 file_manager = ExitStack()
 atexit.register(file_manager.close)
-ref = importlib_resources.files('pygaps.data') / 'default.db'
+ref = importlib_resources_files('pygaps.data') / 'default.db'
 DATABASE = file_manager.enter_context(importlib_resources.as_file(ref))
 
 # Lists of pygaps data
@@ -36,7 +44,7 @@ def load_data():
     ADSORBATE_LIST.extend(adsorbates_from_db(verbose=False))
 
 
-_kernel_res = importlib_resources.files('pygaps.data.kernels')
+_kernel_res = importlib_resources_files('pygaps.data.kernels')
 KERNELS = {
     'DFT-N2-77K-carbon-slit': _kernel_res / 'DFT-N2-77K-carbon-slit.csv',
 }
