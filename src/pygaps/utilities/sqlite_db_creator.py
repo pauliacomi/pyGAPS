d5eb7b5868e40cdb482d3fd01635f98490fa68d9
Paul Iacomi <pauliacomi@users.noreply.github.com>
2023-03-02 22:23:24 +0000

    fix: import of importlib.resources < py3.9

--- src/pygaps/utilities/sqlite_db_creator.py
+++ src/pygaps/utilities/sqlite_db_creator.py
@@ -24,10 +24,17 @@ def db_create(path: str, verbose: bool = False):
         db_execute_general(pragma, path, verbose=verbose)
 
     # Get json files
-    from importlib.resources import files as ir_files
+    try:
+        from importlib.resources import files as ir_files
+    except ImportError:
+        # TODO Deprecation after PY<3.9
+        # Use backported `importlib_resources`.
+        from importlib_resources import files as ir_files
 
     # Get and upload adsorbate property types
-    ads_props_json = ir_files('pygaps.data').joinpath('adsorbate_props.json').read_text(encoding='utf8')
+    ads_props_json = ir_files('pygaps.data').joinpath('adsorbate_props.json').read_text(
+        encoding='utf8'
+    )
     ads_props = json.loads(ads_props_json)
     for ap_type in ads_props:
         pgp_sqlite.adsorbate_property_type_to_db(ap_type, db_path=path, verbose=verbose)
