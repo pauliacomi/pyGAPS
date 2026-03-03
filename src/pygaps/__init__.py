38af77f1eaf3e15e4f7fbf789a535a48911efa6a
Paul IACOMI <mail@pauliacomi.com>
2026-03-03 20:41:24 +0000

    fix: import of importlib.util

--- src/pygaps/__init__.py
+++ src/pygaps/__init__.py
@@ -31,19 +31,19 @@ hard_dependencies = ("numpy", "pandas", "scipy")
 soft_dependencies = {"CoolProp": "Used for many thermodynamic backend calculations."}
 missing_dependencies = []
 
-import importlib.util
+import importlib.util as importlib_util
 for dependency in hard_dependencies:
-    if not importlib.util.find_spec(dependency):
+    if not importlib_util.find_spec(dependency):
         missing_dependencies.append(dependency)
 
 if missing_dependencies:
     raise ImportError(f"Missing required dependencies {missing_dependencies}")
 
 for dependency, reason in soft_dependencies.items():
-    if not importlib.util.find_spec(dependency):
+    if not importlib_util.find_spec(dependency):
         logger.warning(f"Missing important package {dependency}. {reason}")
 
-del importlib.util
+del importlib_util
 del dependency, hard_dependencies, soft_dependencies, missing_dependencies
 
 # Data
